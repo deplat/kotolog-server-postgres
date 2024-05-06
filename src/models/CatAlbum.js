cfc561e91a4450d028c34549c85f484359f6324d
deplat <146228938+deplat@users.noreply.github.com>
2024-05-06 22:25:32 +0300

    Update CatAlbum.js

--- src/models/CatAlbum.js
+++ src/models/CatAlbum.js
@@ -1,7 +1,7 @@
 import dbKotolog from "../database.js"
 import {DataTypes} from "sequelize"
 
-export const CatAlbum = dbKotolog.define('album',{
+export const CatAlbum = dbKotolog.define('cat_album',{
   catId: {
     type: DataTypes.UUID,
     primaryKey: true
