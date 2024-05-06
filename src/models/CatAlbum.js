044d815c4da9201d2b746e3068c9b83a32da400b
deplat <146228938+deplat@users.noreply.github.com>
2024-05-06 19:36:58 +0300

    Update CatAlbum.js

--- src/models/CatAlbum.js
+++ src/models/CatAlbum.js
@@ -4,12 +4,10 @@ import {DataTypes} from "sequelize"
 export const CatAlbum = dbKotolog.define('album',{
   catId: {
     type: DataTypes.UUID,
-    allowNull: false,
     primaryKey: true
   },
   photos: {
     type: DataTypes.ARRAY(DataTypes.STRING),
-    defaultValue: [],
-    allowNull: true
+    defaultValue: []
   }
 })
